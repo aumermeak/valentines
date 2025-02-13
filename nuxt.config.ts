55bc3270d0df842091057e7f3b23543f094820bb
aumermeak <mik@Mik>
2025-02-13 19:29:48 +0800

    second try

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
   app: {
-    baseURL: '/nuxt-github-pages/',
+    baseURL: '/valentines/',
     buildAssetsDir: 'assets',
 
   postcss: {
