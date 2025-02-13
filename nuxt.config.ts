9e842446e7e74f7cba91a5be05d5c70bbdf46be0
aumermeak <mik@Mik>
2025-02-13 20:01:41 +0800

    fourth

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  modules: ['@nuxtjs/tailwindcss'],
   devtools: { enabled: true },
-  css: ['~/assets/css/main.css'],
-  ssr: false,
-  target: 'static',
+  css: ['~/assets/css/tailwind.css'],
   app: {
     baseURL: '/valentines/',
     buildAssetsDir: 'assets',
